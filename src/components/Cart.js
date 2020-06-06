54541c70ae1493f364f670c6c1daacbb6fcfb15f
Shyam <t7bhsh00@students.oamk.fi>
2020-06-07 01:00:06 +0300

    Cart.js updated for adding items to cart and updated styles for cart items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import formatCurrency from "../util";
 
 export default class Cart extends Component {
     render() {
@@ -12,7 +13,27 @@ export default class Cart extends Component {
                    You have {addedProductsInCart.length} Products in the cart{" "}                  
                 </div>
              )}
-           </div>
-        );
-    }      
+          <div className="cart">
+          <ul className="cart-items">
+            {addedProductsInCart.map(item =>(
+              <li key={item._id}>
+                 <div>
+                    <img src={item.image} alt={addedProductsInCart.title}></img>
+                    </div>
+                   <div>
+                      <div>{item.title}</div>
+                    <div className="right">
+                      {formatCurrency(item.price)} x {item.count} {" "}
+                        <button onClick={()=>this.props.removefromCart(item)}>
+                         Remove
+                        </button>
+                    </div>
+                  </div>
+                </li>
+             ))}
+          </ul>
+        </div>
+      </div> 
+     );
+ }      
 }
